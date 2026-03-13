2da32a3cee34d594e7a43d902f55cb4e10999f25
Usama <usama.saleem@geniteam.pk>
2026-03-13 16:12:55 +0500

    Delete storyblok.ts

--- lib/storyblok.ts
+++
@@ -1,9 +0,0 @@
-import { storyblokInit, apiPlugin } from '@storyblok/react/rsc'
-
-export const { storyblokApi } = storyblokInit({
-  accessToken: process.env.NEXT_PUBLIC_STORYBLOK_ACCESS_TOKEN,
-  use: [apiPlugin],
-  apiOptions: {
-    region: 'us',
-  },
-})
